534b2acf824b9491a65393d78b4c08dd7f39232a
enoatu <acro1452@gmail.com>
2021-08-31 11:19:18 +0900

    :wrench: rename file

--- install.js
+++ install.js
@@ -4,22 +4,23 @@ const Rsync = require('rsync')
 const unzipper = require('unzipper')
 const { https } = require('follow-redirects')
 
-// const unzipper = require('node-unzip-2')
+const parentDir = path.dirname(process.execPath).split(path.sep).pop()
 const modsPath = path.dirname(process.execPath)
 const dirPath = path.dirname(process.execPath).split(path.sep)
 dirPath.push('Localizations')
+
 const localizationsPath = dirPath.join(path.sep)
 
-const parentDir = path.dirname(process.execPath).split(path.sep).pop()
-
 const DOWNLOAD_URL = 'https://drive.google.com/u/0/uc?id=1YlImR228V1Wo_s-QvNACZkxZ4TTi1yJH&export=download'
+
 if (parentDir == 'Mods') {
+  console.log('日本語化されたLocalizationファイルをダウンロード中...')
   https.get(DOWNLOAD_URL, (res) => {
     if (res.statusCode !== 200) {
-      throw new Error(DOWNLOAD_URL + ' ' + res.statusMessage);
-    } else {
-      console.log(DOWNLOAD_URL + ' is finished downloaded.');
+      throw new Error(DOWNLOAD_URL + ' ' + res.statusMessage)
     }
+    console.log('ダウンロードが完了しました')
+    console.log('ファイルをsync中...')
     res.pipe(unzipper.Extract({ path: localizationsPath }))
     res.on("end", function() {
       new Rsync()
@@ -30,10 +31,13 @@ if (parentDir == 'Mods') {
           if (err) {
             console.error(err, code, cmd)
           }
+          fs.unlinkSync(process.execPath)
+          fs.rmdirSync(localizationsPath, { recursive: true })
+          console.log('日本語化が完了しました！プレイを楽しんで！')
           process.exit(1)
         })
     })
-   })
+  })
 } else {
   console.log('Modsフォルダにこの実行ファイルを移動させて、再度実行してください')
 }
